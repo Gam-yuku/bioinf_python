09d51e70486933bb8c6c1660be699937a0824c18
Yana <65633709+Gam-yuku@users.noreply.github.com>
2022-06-07 07:43:58 +0300

    Delete data-model objects directory

--- data-model objects/1_2_6_type_object.py
+++
@@ -1,4 +0,0 @@
-x = [1,2,3]
-print(type(x)) #типы в пайтон также являются объектами
-print(type(4))
-print(type(type(x))) 
--- data-model objects/1_2_9_mutable_type.py
+++
@@ -1,9 +0,0 @@
-objects = [1, 3, 'ff', 3, [1], [1], 3]
-new_objects = []
-ans = 0
-
-for obj in objects:
-    if id(obj) not in new_objects:
-        ans += 1
-        new_objects.append(id(obj))
-print(ans)
